5cdee2f359c182a861d5ce0e9abf791e350078fd
Sato08 <nguyentuoik61@gmail.com>
2019-06-27 09:12:39 +0700

    Update

--- product_user/views.py
+++ product_user/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+def product(request):
+    productId = request.POST.get('productId','1001')
+    productName = request.POST.get('productName', 'world')
+    productPrice = request.POST.get('productPrice', '12000')
+    return render(request, 'product.html',
+    {'productId': productId, 'productName': productName, 'productPrice': productPrice})
+
+def index(request):
+    products = [
+        {
+        'productId':'1001',
+        'productName': 'MISC',
+        'productPrice': '12000'
+    },
+    {
+        'productId':'1002',
+        'productName': 'HANDY',
+        'productPrice': '11000'
+    }
+]
+    return render(request, 'index.html', {'products' : products})
+   
